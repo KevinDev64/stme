c8ee9af1f5862e0f316c524ef3d68e46faada78a
KevinDev64 <kevindev56@yandex.ru>
2024-11-04 23:56:22 +0300

    feat: add new functions for beautiful coding

--- stme-compiler.py
+++ stme-compiler.py
@@ -4,11 +4,14 @@ import argparse
 
 # TODO: import some to source and some to config
 null_symbol = '#'
-prefix_lenght = 5
-suffix_lenght = 5
+prefix_lenght = 15
+suffix_lenght = 15
 end_state = 'q0'
 init_state = 'q1'
 commands = []
+tape = []
+start_pos = 0
+is_other_start_pos = False
 
 with open('docs/description.txt') as description_file:
     description = description_file.read()
@@ -24,34 +27,41 @@ source_file.close()
 
 for line in source:
     line = line.split()
-    match line[0]:
-        case 'start_pos':
-            start_pos = int(line[1])
-        case 'tape':
-            tape_str = null_symbol * prefix_lenght + line[1] + null_symbol * suffix_lenght
-            tape = []
-            for char in tape_str:
-                tape.append(char)
-        case 'com':
-            init_state = line[1]
-            init_value = line[2]
-            final_state = line[3]
-            final_value = line[4]
-            com = [init_state, init_value, final_state, final_value]
-            if len(line) == 6: cursor = line[5]; com.append(cursor)
-            else: com.append('C')
-            commands.append(com)
+    if len(line) != 0:
+        match line[0]:
+            case 'null_symbol':
+                null_symbol = line[1]
+            case 'prefix_lenght':
+                prefix_lenght = int(line[1])
+            case 'suffix_lenght':
+                suffix_lenght = int(line[1])
+            case 'start_pos':
+                start_pos = int(line[1])
+                is_other_start_pos = True
+            case 'tape':
+                tape_str = null_symbol * prefix_lenght + line[1] + null_symbol * suffix_lenght
+                tape = []
+                print(tape_str)
+                for char in tape_str:
+                    tape.append(char)
+            case 'com':
+                com_init_state = line[1]
+                com_init_value = line[2]
+                com_final_state = line[3]
+                com_final_value = line[4]
+                com = [com_init_state, com_init_value, com_final_state, com_final_value]
+                if len(line) == 6: cursor = line[5]; com.append(cursor)
+                else: com.append('C')
+                commands.append(com)
 
 current_pos = len(tape) - suffix_lenght - 1
-current_pos = start_pos + prefix_lenght
+if is_other_start_pos: current_pos = start_pos + prefix_lenght - 1
+
 current_state = init_state
 states = set([end_state, init_state])  
 for command in commands:
     states.add(command[0])
     states.add(command[2])
-
-#TODO: states verification
-# print('STATES', states)
 
 # steps = 0 #TODO
 while current_state != end_state:
